0c7f2aaa1adf4804acd20343ddd573a9760cdb36
Piyush <piyushagarwal.0108@gmail.com>
2019-06-15 07:07:12 +0000

    socket in p3

---
+++ receiverP3_p11.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import socket
+re_ip="127.0.0.1"
+re_port=4484  # 0 - 1024 -- you can check free udp port : netstat -nulp
+
+# Creating UDP socket
+#		  ip type v4	   UDP
+s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+
+# fitting ip and port with UDP socket
+s.bind((re_ip,re_port))
+print("To Close communication reply the sender with a blank message")
+while(True):
+
+	# receiver data from sender
+	data=s.recvfrom(150)
+	print('Server says: '+data[0].decode('ascii'))
+	text = input('Client says: ')
+# Converting str to bytes - like object
+	s.sendto(text.encode('ascii'),data[1])
+	if len(data[0]) == 0:
+		s.sendto('',data[1])
+		break
+
+s.close()
---
+++ senderP3_p11.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python2
+
+import socket
+re_ip="127.0.0.1"
+re_port=4484  # 0 - 1024 -- you can check free udp port : netstat -nulp
+
+# Creating UDP socket
+#		  ip type v4	   UDP
+s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+print("To close communication send an blank message")
+# Sending data to target
+while(True):
+	text = input('Server says: ').encode('ascii')
+	s.sendto(text,(re_ip,re_port))
+	data = s.recvfrom(100)
+	print('Client says: '+data[0].decode('ascii'))
+	re_ip,re_port = data[1]
+	if len(data[0]) == 0:
+		s.sendto('',(re_ip,re_port))
+		break
+s.close()
