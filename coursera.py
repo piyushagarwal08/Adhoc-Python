8287b52a162ca415712b8bd51e38c550a31690b2
Piyush <piyushagarwal.0108@gmail.com>
2019-08-22 19:21:27 +0530

    reached till application form

--- coursera.py
+++ coursera.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait 
 from selenium.webdriver.support import expected_conditions as EC 
 from selenium.webdriver.chrome.options import Options 
+from selenium.webdriver.common.by import By
 import time
 import pyautogui as pg
 
@@ -19,50 +20,57 @@ options.add_extension("/usr/bin/extension_1_5_0_0.crx")
 driver = webdriver.Chrome(executable_path="/usr/bin/chromedriver",options=options)  # opens the chrome browser
 #driver = webdriver.Chrome(options=options)
 def coursera():
-    # opens google chrome
-    #driver = webdriver.Chrome(executable_path="/usr/bin/chromedriver")  # opens the chrome browser
     driver.maximize_window()
     # a timer for all the commands to keep things smooth
     driver.implicitly_wait(10)
     # open coursera 
     driver.get("https://www.coursera.org")  # opens the webpage
-    time.sleep(120)
+    time.sleep(7)
     
     print("time to add cookie")
     # Add coursera cookie
-    pg.click(1797,131)  # select cookie extension
+    pg.click(1797,119)  # select cookie extension
     time.sleep(1)
-    pg.click(1554,117) # select import cookie button
+    pg.click(1416,144) # select import cookie button
     time.sleep(1)
-    pg.click(1404,245) # click on text input field
+    pg.click(1289,316) # click on text input field
     time.sleep(1)
     pg.typewrite(cookie)  # enter coursera cookies
     time.sleep(1)
-    pg.click(1547,687)  # save the cookies
+    pg.click(1424,894)  # save the cookies
 
-    time.sleep(8)
+    time.sleep(3)
     # now refresh the page 
     driver.refresh()
 
-    # open coursera 
-    # driver.get("https://www.coursera.org")  # opens the webpage
-
     print("Opened the Webpage:",driver.title,"with url: ",driver.current_url)
+    # Code to find a course
+    wait = WebDriverWait(driver,10)
+
+    course_field = wait.until(EC.element_to_be_clickable((By.XPATH,'//*[@id="rendered-content"]/div/div/span/div[2]/header/div[2]/div/div[1]/div/div/div[2]/div/div[2]/form/div/div/div[1]/div/input')))
+    course_field.clear()
+    course_field.send_keys(course) # types in the course
+    time.sleep(3)
+    print('time to select the course')
+    pg.click(629,335) # selects the course
+    driver.find_element_by_id('finaid_button').click()  # click on financial update available
+    time.sleep(1)
+    driver.find_element_by_id('financial_aid_modal_apply_button').click()  # continue to financial aid
 
 coursera()
 
-# Code to find a course
 wait = WebDriverWait(driver,10)
+check_box1 = wait.until(EC.element_to_be_clickable((By.ID,'info_checkbox')))  # wait for first check box to get in action
+time.sleep(1)
+check_box1.click()  # click on 1st checkbox
+time.sleep(1)
 
-course_field = wait.until(EC.element_to_be_clickable((By.XPATH,'//*[@id="rendered-content"]/div/div/span/div[2]/header/div[2]/div/div[1]/div/div/div[2]/div/div[2]/form/div/div/div[1]/div/input')))
-course_field.clear().send_keys(course) # types in the course
-time.sleep(3)
-driver.find_element_by_xpath('//*[@id="rendered-content"]/div/div/span/div[2]/header/div[2]/div/div[1]/div/div/div[2]/div/div[2]/form/div/div/div[1]/button[2]/div').click()
+driver.find_element_by_id('//*[@id="info_checkbox"]').click() # click on 2nd checkbox  
+time.sleep(1)
 
-time.sleep(10)
-# time to select the course
+driver.find_element_by_id('accept-terms-field').send_keys('I agree to the terms above') # enter agree terms in text field
+time.sleep(1)
 
-
-
-
+application_button = wait.until(EC.element_to_be_clickable(By.ID,'continue_finaid_application_button'))
+application_button.click() # clicks on application continue button
 
