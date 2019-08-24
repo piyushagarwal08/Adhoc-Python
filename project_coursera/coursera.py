f91086227ab2d9048f327254949dbb41e69b795d
Piyush <piyushagarwal.0108@gmail.com>
2019-08-24 07:58:57 +0530

    finally time for testing coursera

--- project_coursera/coursera.py
+++ project_coursera/coursera.py
@@ -8,13 +8,14 @@ import pyautogui as pg
 from selenium.webdriver.support.ui import Select
 
 
-email = 'piyushagarwal.0108@gmail.com'
-print("Enter your password: ")
-password = 'qwerty12345'
+email = input('Please enter your email: ')
+password = input('Please enter your password: ')
 
-course = 'Python for Everybody'
-cookie_file = open('cookies.txt')
+course = input('Please enter the exact name of the course for best results: ')   # pls enter your course name
+cookie_file = open('cookies.txt')  # account cookies should be saved with same name
 cookie = cookie_file.read()
+cookie_file.close()
+
 
 options = Options()
 options.add_extension("/usr/bin/extension_1_5_0_0.crx")
@@ -26,7 +27,7 @@ def coursera():
     driver.implicitly_wait(10)
     # open coursera 
     driver.get("https://www.coursera.org")  # opens the webpage
-    time.sleep(7)
+    time.sleep(3)
     
     print("time to add cookie")
     # Add coursera cookie
@@ -71,8 +72,45 @@ def coursera():
     application_button = wait.until(EC.element_to_be_clickable((By.ID,'continue_finaid_application_button')))
     application_button.click() # clicks on application continue button
 
+    education = Select(driver.find_element_by_id('finaid-educationalBackground')) # instance of educational background
+    education.select_by_value('SOME_COLLEGE')  # select some college 
+    print(type(education))
+
+    time.sleep(1)
+    annual_income = driver.find_element_by_id('finaid-income')
+    annual_income.clear()
+    annual_income.send_keys('1023')  # enter in the field of annual income
+
+    time.sleep(1)
+    employment = Select(driver.find_element_by_id('finaid-employmentStatus'))  # instance of employment status
+    employment.select_by_value('STUDENT') # select student as status
+
+    time.sleep(1)
+    payable = driver.find_element_by_id('finaid-amount-can-pay')   # possible amount we can pay set to 0
+    payable.clear()
+    payable.send_keys('0')
+
+    time.sleep(1)
+    file1 = open('para1')
+    para1 = file1.read()
+    driver.find_element_by_id('finaid-reason').send_keys(para1)  # first paragraph filled successfully
+    file1.close()
+
+    time.sleep(1)
+    file2 = open('para2')
+    para2 = file2.read()
+    driver.find_element_by_id('finaid-goal').send_keys(para2)    # second paragraph filled
+    file2.close()
+
+    time.sleep(1)
+    driver.find_element_by_id('finaid-loanReason').send_keys('Currently i have no source of income')  # third paragraph filled
+
+    print('Filled the finally form also successfully')
+
+    # finally to submit application
+    driver.find_element_by_id('submit_application_button').click()
+
+    driver.quit()
 
 coursera()
-
-
 
